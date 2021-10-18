01d67d9c4ac330bd5f220d88d997516c098028fb
Kadal <riz.idn.dev@gmail.com>
2021-10-18 22:10:22 +0700

    revamp display

--- app/Views/selesai.php
+++ app/Views/selesai.php
@@ -5,6 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GLN GAREULIS - Tugas Peserta Diklat</title>
+    
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url('favicons/apple-touch-icon.png');?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url('favicons/favicon-32x32.png');?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url('favicons/favicon-16x16.png');?>">
+    <link rel="manifest" href="<?php echo base_url('favicons/site.webmanifest');?>">
+    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
@@ -15,11 +21,12 @@
     <div class="container p-4">
 
         <div class="py-4 text-center">
+            <img src="<?= base_url();?>/logo.png" alt="logo" style="width:20rem; height:20rem;">
             <h1>Terimakasih telah mengisi form</h1>
         </div>
 
         <div class="container">
-            <a href="<?php echo route_to('/');?>" class="btn btn-primary btn-block text-decoration-none">Isi kembali ?</a>
+            <a href="<?php echo route_to('/');?>" class="btn btn-primary btn-block text-decoration-none">Kembali</a>
         </div>
 
     </div>
