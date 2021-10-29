8ce34375103b899c9a28d035e5736c5ae216c327
Kadal <riz.idn.dev@gmail.com>
2021-10-29 12:16:37 +0700

    initial fix

---
+++ public/404.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GLN GAREULIS - Dokumen tidak ditemukan</title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+    <link rel="manifest" href="favicons/site.webmanifest">
+
+    <style>
+        .errorgls {
+            display : block;
+            max-width: 100%;
+            height: auto;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    </style>
+
+</head>
+<body class="bg-light">
+    <div class="container p-4">
+
+        <div class="text-center">
+            <img src="404.png" alt="logo" class="errorgls">
+        </div>
+
+    </div>
+
+</body>
+</html>
